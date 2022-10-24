39a7af7b585f21753718dab1a860cc2d75babc6e
vladd12 <svm@asu-vei.ru>
2022-10-24 10:45:06 +0300

    [*] Updating searching kernel headers [+] Added .gitignore

--- cmake/Findkernel-headers.cmake
+++ cmake/Findkernel-headers.cmake
@@ -1,30 +1,22 @@
 # Linux kernel version
-execute_process(
-        COMMAND uname -r
-        OUTPUT_VARIABLE KERNEL_RELEASE
-        OUTPUT_STRIP_TRAILING_WHITESPACE)
+execute_process(COMMAND uname -r
+    OUTPUT_VARIABLE KERNEL_RELEASE
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
 
 # Find the headers
 find_path(KERNELHEADERS_DIR
-        include/linux/user.h
-        PATHS
-        # RedHat derivatives
-        /usr/src/kernels/${KERNEL_RELEASE}
-        # Debian derivatives
-        /usr/src/linux-headers-${KERNEL_RELEASE})
-
-message(STATUS "Kernel release: ${KERNEL_RELEASE}")
-message(STATUS "Kernel headers: ${KERNELHEADERS_DIR}")
+    include/generated/uapi/linux/version.h
+    PATHS
+    # RedHat derivatives
+    /usr/src/kernels/${KERNEL_RELEASE}
+    # Debian derivatives
+    /usr/src/linux-headers-${KERNEL_RELEASE})
 
 if (KERNELHEADERS_DIR)
-    set(KERNELHEADERS_INCLUDE_DIRS
-            ${KERNELHEADERS_DIR}/include/generated/uapi
-            CACHE PATH "Kernel headers include dirs"
-            )
-    set(KERNELHEADERS_FOUND 1 CACHE STRING "Set to 1 if kernel headers were found")
-    include_directories(${KERNELHEADERS_INCLUDE_DIRS})
-else (KERNELHEADERS_DIR)
-    set(KERNELHEADERS_FOUND 0 CACHE STRING "Set to 1 if kernel headers were found")
-endif (KERNELHEADERS_DIR)
+    set(KERNELHEADERS_INCLUDE_DIRS ${KERNELHEADERS_DIR}/include/generated/uapi)
+    set(KERNELHEADERS_FOUND 1)
+else ()
+    set(KERNELHEADERS_FOUND 0)
+endif ()
 
 mark_as_advanced(KERNELHEADERS_FOUND)
