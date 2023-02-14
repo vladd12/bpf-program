209b13e59796c6a1468bb8b47bd9de8b4ba57b83
vladd12 <svm@asu-vei.ru>
2023-02-14 17:56:48 +0300

    [*] build scripts update

---
+++ tools/docker-cross/README.md
@@ -0,0 +1,8 @@
+# bcc versions
+
+- (v0.17)[https://github.com/iovisor/bcc/tree/ad5b82a5196b222ed2cdc738d8444e8c9546a77f], kernel version 5.9
+- (v0.18)[https://github.com/iovisor/bcc/tree/b1ab869032611d9fcdaea56851cd6126cca2eba8], kernel version 5.10
+- (v0.19)[https://github.com/iovisor/bcc/tree/4c561d037e2798563c2e87edcc5a406b020a458c], kernel version 5.11
+- (v0.20)[https://github.com/iovisor/bcc/tree/14278bf1a52dd76ff66eed02cc9db7c7ec240da6], kernel version 5.12
+- (v0.21)[https://github.com/iovisor/bcc/tree/321c9c979889abce48d0844b3d539ec9a01e6f3c], kernel version 5.13
+
--- tools/docker-cross/run.sh
+++ tools/docker-cross/run.sh
@@ -5,28 +5,21 @@ docker run --platform=linux/amd64 -i -t cross_amd64
 # Getting Docker container ID
 docker ps 
 cd C:/Temp/files
-#ln -s /path/to/file /path/to/symlink
 docker cp <ID>:/home/user/bpf-program/build/bpf_test bpf_test
 docker cp <ID>:/home/user/bpf-program/build/bpf .
 
 docker cp <ID>:/usr/lib/arm-linux-gnueabihf/libbpf.a libbpf.a
-#docker cp <ID>:/usr/lib/arm-linux-gnueabihf/libbpf.so libbpf.so
-#docker cp <ID>:/usr/lib/arm-linux-gnueabihf/libbpf.so.1 libbpf.so.1
 docker cp <ID>:/usr/lib/arm-linux-gnueabihf/libbpf.so.1.1.0 libbpf.so.1.1.0
 ln -s ./libbpf.so.1.1.0 ./libbpf.so.1
 ln -s ./libbpf.so.1.1.0 ./libbpf.so
 
 docker cp <ID>:/usr/lib/libbcc-loader-static.a libbcc-loader-static.a
 docker cp <ID>:/usr/lib/libbcc.a libbcc.a
-#docker cp <ID>:/usr/lib/libbcc.so libbcc.so
-#docker cp <ID>:/usr/lib/libbcc.so.0 libbcc.so.0
 docker cp <ID>:/usr/lib/libbcc.so.0.25.0 libbcc.so.0.25.0
 ln -s ./libbcc.so.0.25.0 ./libbcc.so.0
 ln -s ./libbcc.so.0.25.0 ./libbcc.so
 
 docker cp <ID>:/usr/lib/libbcc_bpf.a libbcc_bpf.a
-#docker cp <ID>:/usr/lib/libbcc_bpf.so libbcc_bpf.so
-#docker cp <ID>:/usr/lib/libbcc_bpf.so.0 libbcc_bpf.so.0
 docker cp <ID>:/usr/lib/libbcc_bpf.so.0.25.0 libbcc_bpf.so.0.25.0
 ln -s ./libbcc_bpf.so.0.25.0 ./libbcc_bpf.so.0
 ln -s ./libbcc_bpf.so.0.25.0 ./libbcc_bpf.so
