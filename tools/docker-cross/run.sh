da81ad9f0f4d1c806f2408c49e1ec2b281dc9578
vladd12 <svm@asu-vei.ru>
2023-02-17 11:45:16 +0300

    [↓] version bcc downgrade

--- tools/docker-cross/run.sh
+++ tools/docker-cross/run.sh
@@ -9,20 +9,20 @@ docker cp <ID>:/home/user/bpf-program/build/bpf_test bpf_test
 docker cp <ID>:/home/user/bpf-program/build/bpf .
 
 docker cp <ID>:/usr/lib/arm-linux-gnueabihf/libbpf.a libbpf.a
-docker cp <ID>:/usr/lib/arm-linux-gnueabihf/libbpf.so.1.1.0 libbpf.so.1.1.0
-ln -s ./libbpf.so.1.1.0 ./libbpf.so.1
-ln -s ./libbpf.so.1.1.0 ./libbpf.so
+docker cp <ID>:/usr/lib/arm-linux-gnueabihf/libbpf.so.0.3.0 libbpf.so.0.3.0
+ln -s ./libbpf.so.0.3.0 ./libbpf.so.0
+ln -s ./libbpf.so.0.3.0 ./libbpf.so
 
 docker cp <ID>:/usr/lib/libbcc-loader-static.a libbcc-loader-static.a
 docker cp <ID>:/usr/lib/libbcc.a libbcc.a
-docker cp <ID>:/usr/lib/libbcc.so.0.25.0 libbcc.so.0.25.0
-ln -s ./libbcc.so.0.25.0 ./libbcc.so.0
-ln -s ./libbcc.so.0.25.0 ./libbcc.so
+docker cp <ID>:/usr/lib/libbcc.so.0.17.0 libbcc.so.0.17.0
+ln -s ./libbcc.so.0.17.0 ./libbcc.so.0
+ln -s ./libbcc.so.0.17.0 ./libbcc.so
 
 docker cp <ID>:/usr/lib/libbcc_bpf.a libbcc_bpf.a
-docker cp <ID>:/usr/lib/libbcc_bpf.so.0.25.0 libbcc_bpf.so.0.25.0
-ln -s ./libbcc_bpf.so.0.25.0 ./libbcc_bpf.so.0
-ln -s ./libbcc_bpf.so.0.25.0 ./libbcc_bpf.so
+docker cp <ID>:/usr/lib/libbcc_bpf.so.0.17.0 libbcc_bpf.so.0.17.0
+ln -s ./libbcc_bpf.so.0.17.0 ./libbcc_bpf.so.0
+ln -s ./libbcc_bpf.so.0.17.0 ./libbcc_bpf.so
 
 sudo mkdir /usr/src/linux-headers-$(uname -r)
 cd /usr/src/linux-headers-$(uname -r)
