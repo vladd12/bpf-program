5467be9b0346958dd0eff69b2f8cde5947d4fedb
vladd12 <svm@asu-vei.ru>
2023-03-28 17:41:19 +0300

    [*] Adding commentaries for the FastFile class

--- include/fast_file.h
+++ include/fast_file.h
@@ -4,6 +4,7 @@
 #include <memory>
 #include <string>
 
+/// \brief Structure for closing the captured file's handle.
 struct FileCloser
 {
     void operator()(FILE *fHandle)
@@ -12,21 +13,31 @@ struct FileCloser
     }
 };
 
-using FilePtr = std::unique_ptr<FILE, FileCloser>;
+using FilePtr = std::unique_ptr<FILE, FileCloser>; ///< Container for the FILE pointer.
 
+/// \brief Class for creating file objects with fast writing data.
 class FastFile
 {
 private:
     FilePtr file;
 
 public:
+    /// \brief Open a file with the specified filename.
     explicit FastFile(const char *filename);
+
+    /// \brief Open a file with the specified filename.
     explicit FastFile(const std::string &filename);
 
+    /// \brief Writing single character to rhe file.
     bool write(const char ch);
+
+    /// \brief Writing C-style string data to the file.
     bool write(const char *text);
+
+    /// \brief Writing std::string object's data to the file.
     bool write(const std::string &text);
 
+    /// \brief Writing numerical data to the file.
     template <typename T> //
     bool write(const T number)
     {
