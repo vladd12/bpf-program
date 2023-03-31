3671ac9ea9538aa2e04ea605d1dd195ab0123d52
vladd12 <svm@asu-vei.ru>
2023-03-31 10:22:27 +0300

    [+] Adding benchmarks: initial stage [-] Removing useless CMake scripts [*] Update readme

--- README.md
+++ README.md
@@ -4,6 +4,18 @@
 ```
 apt search linux-headers-$(uname -r)
 sudo apt-get install linux-headers-$(your_kernel)
-sudo apt-get install bpfcc-tools libbpfcc libbpfcc-dev
+sudo apt-get install libbpfcc-dev
+```
+
+## Building on machine
+```
+```
+
+## Building with Docker
+### x86
+```
+```
+### arm64
+```
 ```
 
---
+++ cmake/get_gbench.cmake
@@ -0,0 +1,14 @@
+include(FetchContent)
+# Getting Google Benchmark sources from GitHub repository
+FetchContent_Declare(
+    benchmark
+    GIT_REPOSITORY https://github.com/google/benchmark.git
+    GIT_TAG        d572f4777349d43653b21d6c2fc63020ab326db2 # v1.7.1
+)
+
+set(BENCHMARK_ENABLE_GTEST_TESTS OFF)
+set(BENCHMARK_ENABLE_TESTING OFF)
+set(BENCHMARK_ENABLE_INSTALL OFF)
+
+# Compiling Google Benchmark library
+FetchContent_MakeAvailable(benchmark)
